afd29f9a5ad90a8f1b0e5070254a63d72091dafe
Eduardo Reyes <3013805890101@ingenieria.usac.edu.gt>
2023-12-09 09:32:55 -0600

    [HOTFIX] VALOR
    Se corrigio un problema al obtener un tipo que retornaba vacio

--- FUNCIONES/ARBOL/VALOR.py
+++ FUNCIONES/ARBOL/VALOR.py
@@ -5,32 +5,53 @@ class VALOR(Expresion):
         super().__init__(linea, columna, "VALOR")
         self.valor = valor
         self.tipo_valor = tipo_valor
+        if(self.tipo_valor == "INT"):
+                self.tipo = TIPODATO(TIPO.INT)
+
+        elif(self.tipo_valor == "BIT"):
+            self.tipo = TIPODATO(TIPO.BIT)  
+
+        elif(self.tipo_valor == "DECIMAL"):
+            self.tipo = TIPODATO(TIPO.DECIMAL)
+
+        elif(self.tipo_valor == "FECHA"):
+            self.tipo = TIPODATO(TIPO.DATE)
+
+        elif(self.tipo_valor == "FECHAHORA"):
+            self.tipo = TIPODATO(TIPO.DATETIME)
+
+        elif(self.tipo_valor == "CADENA"):
+            self.tipo = TIPODATO(TIPO.VARCHAR)
+            self.tipo.modificar_size(len(self.valor))  
+
+        elif(self.tipo_valor == "CHAR"):   
+            self.tipo = TIPODATO(TIPO.CHAR)
+
+        elif(self.tipo_valor == "VARCHAR"):   
+            self.tipo = TIPODATO(TIPO.VARCHAR)
 
     def obtener_valor(self, actual, globa, ast):
         try:
-            if(self.tipo_valor == "INT"):
-                self.tipo = TIPODATO(TIPO.INT)
+            if(self.tipo.obtener_tipo_dato() == TIPO.INT):
                 return(int(self.valor))
 
-            elif(self.tipo_valor == "BIT"):
-                self.tipo = TIPODATO(TIPO.BIT)
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.BIT):
                 return(int(self.valor))
             
-            elif(self.tipo_valor == "DECIMAL"):
-                self.tipo = TIPODATO(TIPO.DECIMAL)
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.DECIMAL):
                 return(float(self.valor))
 
-            elif(self.tipo_valor == "FECHA"):
-                self.tipo = TIPODATO(TIPO.DATE)
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.DATE):
                 return(str(self.valor))
 
-            elif(self.tipo_valor == "FECHAHORA"):
-                self.tipo = TIPODATO(TIPO.DATETIME)
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.DATETIME):
                 return(str(self.valor))
 
-            elif(self.tipo_valor == "CADENA"):
-                self.tipo = TIPODATO(TIPO.VARCHAR)
-                self.tipo.modificar_size(len(self.valor))  
-                return str(self.valor)          
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.VARCHAR):
+                return str(self.valor) 
+            
+            elif(self.tipo.obtener_tipo_dato() ==TIPO.CHAR):
+                return str(self.valor)  
+                
         except:
             print("NO SE PUEDE OBTENER EL VALOR DE LA EXPRESION")
--- FUNCIONES/FUNCIONES_SISTEMA/SUBSTRAER.py
+++ FUNCIONES/FUNCIONES_SISTEMA/SUBSTRAER.py
@@ -10,20 +10,31 @@ class SUBSTRAER(Expresion):
 
     def obtener_valor(self, actual, globa, ast):
         if(isinstance(self.inicio,VALOR) and isinstance(self.final,VALOR) and isinstance(self.cadena1,VALOR)):
-            num1 = self.inicio.obtener_valor(actual,globa,ast) - 1 
-            if(num1<0):
-                num1 = 0
+            num1 = self.inicio.obtener_valor(actual,globa,ast) - 1  #-1 porque en python el inicio es 0 y en el proyecto es 1
             num2 = self.final.obtener_valor(actual,globa,ast)
             cadena = self.cadena1.obtener_valor(actual,globa,ast)
-            val_respuesta = ""
-            #VALIDACION SI EL NUMERO FINAL EXCEDE LA CANTIDAD DE CARACTERES 
-            if(num2 > len(cadena)):
-                ast.escribir_en_consola("ERROR: El numero final, excede la cantidad de caracteres de la cadena!\n")
-                respuesta = VALOR("",TIPO.ERROR,self.linea,self.columna)
-                return respuesta
-            for i in range (num1, num2):
-                val_respuesta += cadena[i]
+
+            #tipo_inicio = self.inicio.tipo.obtener_tipo_dato()
+            tipo_inicio = self.inicio.tipo.obtener_tipo_dato()
+            tipo_final = self.final.tipo.obtener_tipo_dato()
+            tipo_cadena = self.cadena1.tipo.obtener_tipo_dato()
+
+            #SI INICIO Y FINAL SON ENTEROS O BIT(1 TAMBIEN ES ENTERO), Y CADENA ES CHAR O VARCHAR
+            if((tipo_inicio == TIPO.INT or tipo_inicio == TIPO.BIT)and (tipo_final== TIPO.INT or tipo_final == TIPO.BIT)and (tipo_cadena == TIPO.CHAR or tipo_cadena == TIPO.VARCHAR) ):
+                #EXTRA SI EL INICIO EN MENOR A 0 SE PONE POSICION 0
+                
+                if(num1<0):
+                    num1 = 0
             
-            respuesta = VALOR(val_respuesta,TIPO.VARCHAR,self.linea,self.columna)
-            ast.escribir_en_consola("LA NUEVA CADENA ES: "+respuesta.valor +"\n")
-            return respuesta
+                val_respuesta = ""
+                #VALIDACION SI EL NUMERO FINAL EXCEDE LA CANTIDAD DE CARACTERES 
+                if(num2 > len(cadena)):
+                    ast.escribir_en_consola("ERROR: El numero final, excede la cantidad de caracteres de la cadena!\n")
+                    respuesta = VALOR("",TIPO.ERROR,self.linea,self.columna)
+                    return respuesta
+                for i in range (num1, num2):
+                    val_respuesta += cadena[i]
+                
+                respuesta = VALOR(val_respuesta,TIPO.VARCHAR,self.linea,self.columna)
+                ast.escribir_en_consola("LA NUEVA CADENA ES: "+respuesta.valor +"\n")
+                return respuesta
